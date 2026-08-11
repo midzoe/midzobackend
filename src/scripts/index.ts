3fd662371c295f127dc65f72b8183fa35090bd5c
barry thierno alhassane <barry.thierno@digitalexpertize.com>
2026-08-11 18:11:36 +0000

    update

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,5 +5,3 @@ export * from './seed-language-centers';
 export * from './seed-orientation';
 export * from './seed-tourism-events';
 export * from './seed-tourism-programs';
-export * from './tmp-check-programs';
-export * from './tmp-reset-programs';
